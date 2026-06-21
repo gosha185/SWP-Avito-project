82b003077e4d0f140d4cd01b41c28998fdef838c
gosha185 <sergeevge2019@yandex.ru>
2026-06-21 21:17:15 +0300

    feat(service): Added validation and idempotency check

---
+++ itmo_ledger/internal/service/validation.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
+	"simple-ledger.itmo.ru/internal/models"
+	"simple-ledger.itmo.ru/internal/storage"
+)
+
+func (bs *BonusService) validate(ctx context.Context, user uuid.UUID, requiredPoints int64, key string, positiveIntegers ...int64) (*sql.Tx, *models.Balance, error) {
+	tx, err := bs.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	balance, err := bs.BalancesDB.GetBalanceForUpdate(ctx, tx, user)
+	if err != nil {
+		tx.Rollback()
+		return tx, nil, err
+	}
+	temp, err := bs.LedgersDB.GetByExternalKey(ctx, key)
+	if err != nil {
+		tx.Rollback()
+		return tx, balance, err
+	}
+	if temp != nil {
+		tx.Rollback()
+		return tx, balance, storage.ErrLedgerDuplicate
+	}
+	for _, i := range positiveIntegers {
+		if i < 1 {
+			tx.Rollback()
+			return tx, balance, storage.ErrIncorrectInput
+		}
+	}
+	if balance.Available < requiredPoints {
+		tx.Rollback()
+		return tx, balance, storage.ErrInsufficientBalance
+	}
+	return tx, balance, nil
+}
