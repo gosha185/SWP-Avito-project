b026e244888a68a2d8f52d6989c34e93633a66bf
gosha185 <sergeevge2019@yandex.ru>
2026-07-01 04:07:41 +0300

    fix(service): Fixed CancelAllExpiredHolds

--- bonus_service/internal/service/cancelAllExpiredHolds.go
+++ bonus_service/internal/service/cancelAllExpiredHolds.go
@@ -3,44 +3,47 @@ package service
 import (
 	"bonus-service/internal/models"
 	"context"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"time"
 )
 
 func (bs *BonusService) CancelAllExpiredHolds(ctx context.Context) error {
-	tx, err := bs.DB.BeginTx(ctx, nil)
+	holds, err := bs.HoldsDB.GetExpiredHolds(ctx)
 	if err != nil {
 		return err
 	}
-	defer tx.Rollback()
-	entry := &models.LedgerEntry{OperationType: models.OpExpiry, CreatedAt: time.Now(), Metadata: json.RawMessage(`"expired": "holds"`)}
-	holds, err := bs.HoldsDB.GetExpiredHolds(ctx, tx)
-	if err != nil {
-		return err
-	}
+	var tx *sql.Tx
 	for _, hold := range holds {
-		_, err := bs.BalancesDB.GetBalanceForUpdate(ctx, tx, hold.UserID)
+		tx, err = bs.DB.BeginTx(ctx, nil)
 		if err != nil {
-			return err
+			break
+		}
+		entry := &models.LedgerEntry{OperationType: models.OpCancel, Amount: hold.Amount, CreatedAt: time.Now(), Metadata: json.RawMessage(fmt.Sprintf(`{"order_id": "%s", "expires_at": "%s", "expired": "holds"}`, hold.OrderID.String(), hold.ExpiresAt.Format(time.RFC3339)))}
+		_, err = bs.BalancesDB.GetBalanceForUpdate(ctx, tx, hold.UserID)
+		if err != nil {
+			break
 		}
 		err = bs.BalancesDB.UpdateBalance(ctx, tx, hold.UserID, -hold.Amount, 0)
 		if err != nil {
-			return err
+			break
 		}
 		err = bs.HoldsDB.UpdateHoldStatus(ctx, tx, hold.ID, "cancelled")
 		if err != nil {
-			return err
+			break
 		}
-		entry.Amount += hold.Amount
+		err = bs.LedgersDB.Insert(ctx, tx, entry)
+		if err != nil {
+			break
+		}
+		err = tx.Commit()
+		if err != nil {
+			break
+		}
 	}
-	err = bs.LedgersDB.Insert(ctx, tx, entry)
-	if err != nil {
-		return err
+	if tx != nil {
+		defer tx.Rollback()
 	}
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-	_, err = bs.HoldsDB.DeleteOldHolds(ctx, 0)
 	return err
 }
